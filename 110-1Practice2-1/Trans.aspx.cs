bec1c100e133bd801dbcd039657d989ac748439a
于翔 林 <108111120@mail.aeust.edu.tw>
2021-10-01 12:23:28 +0800

    新增d_Value

---
+++ 110-1Practice2-1/Trans.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _110_1Practice2_1
+{
+    public partial class Trans : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            double d_Value = 66;
+
+                double d_Value2 = 0;
+
+                d_Value2 = 5.0 * (d_Value - 32.0) / 9.0;
+
+                Response.Write("d_Value " + "= 66F " + " = 解答 " + d_Value2);
+
+        }
+    }
+}
