edfb13fe465842c1f400c7d5b97e76aa9f54e8c9
于翔 林 <108111120@mail.aeust.edu.tw>
2021-10-01 14:46:32 +0800

    新增d_Value

--- 110-1Practice2-1/Trans.aspx.cs
+++ 110-1Practice2-1/Trans.aspx.cs
@@ -13,11 +13,9 @@ namespace _110_1Practice2_1
         {
             double d_Value = 66;
 
-                double d_Value2 = 0;
+            double d_Value2 = 5.0 * (d_Value - 32.0) / 9.0;
 
-                d_Value2 = 5.0 * (d_Value - 32.0) / 9.0;
-
-                Response.Write("d_Value " + "= 66F " + " = 解答 " + d_Value2);
+                Response.Write("d_Value " + "= 66F " + " = " + d_Value2);
 
         }
     }
