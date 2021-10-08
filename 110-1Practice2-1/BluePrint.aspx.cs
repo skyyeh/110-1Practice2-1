5497ae50dbe95e99d547fdc5ab974e04de8c2889
于翔 林 <108111120@mail.aeust.edu.tw>
2021-10-08 10:33:10 +0800

    新增BluePrint

---
+++ 110-1Practice2-1/BluePrint.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _110_1Practice2_1
+{
+    //或這裡
+    public partial class BluePrint : System.Web.UI.Page
+    {
+        public class Man {
+            public string s_Id;
+            public float f_Hei;
+            public float f_Wei;
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Man o_Man = new Man();
+            o_Man.s_Id = "A123456789";
+            o_Man.f_Hei = 178.5f;
+            o_Man.f_Wei = 72.8f;
+            Response.Write("s_Id = " + o_Man.s_Id + "," + "f_Hei=" + o_Man.f_Hei.ToString() + "," + "f_Wei=" + o_Man.f_Wei.ToString());
+           
+        }
+    }
+}
